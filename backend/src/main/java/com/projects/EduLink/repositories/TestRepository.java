3f6ecc31da2bfaa807f03a63476dd98bba1ccf10
LorenzoBondan <lbbondan@gmail.com>
2023-07-24 14:03:00 -0300

    testService

--- backend/src/main/java/com/projects/EduLink/repositories/TestRepository.java
+++ backend/src/main/java/com/projects/EduLink/repositories/TestRepository.java
@@ -1,11 +1,23 @@
 package com.projects.EduLink.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.projects.EduLink.entities.Subject;
 import com.projects.EduLink.entities.Test;
+import com.projects.EduLink.entities.User;
 
 @Repository
 public interface TestRepository extends JpaRepository<Test,Long>{
 
+	@Query("SELECT obj FROM Test obj WHERE "
+			+ "((obj.student = :user) OR "
+			+ "(obj.student.parents CONTAINS(:user))) AND "
+			+ "(obj.subject = :subject) AND "
+			+ "(obj.subject.team = :team) "
+			+ "ORDER BY obj.date DESC")
+	Page<Test> getTestsFromStudentSubjectTeam(User user, Subject subject, String team, Pageable pageable);
 }
---
+++ backend/src/main/java/com/projects/EduLink/services/TestService.java
@@ -0,0 +1,100 @@
+package com.projects.EduLink.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projects.EduLink.dto.TestDTO;
+import com.projects.EduLink.entities.Subject;
+import com.projects.EduLink.entities.Test;
+import com.projects.EduLink.entities.User;
+import com.projects.EduLink.repositories.SubjectRepository;
+import com.projects.EduLink.repositories.TestRepository;
+import com.projects.EduLink.repositories.UserRepository;
+import com.projects.EduLink.services.exceptions.DataBaseException;
+import com.projects.EduLink.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class TestService {
+
+	@Autowired
+	private TestRepository repository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private SubjectRepository subjectRepository;
+	
+	@Autowired
+	private AuthService authService;
+
+	@Transactional(readOnly = true)
+	public Page<TestDTO> findAllPaged(Pageable pageable) {
+		Page<Test> list = repository.findAll(pageable);
+		return list.map(x -> new TestDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<TestDTO> findTestsFromStudentSubjectTeam(Long subjectId, String team, Pageable pageable) {
+		User me = authService.authenticated();
+		Subject subject = subjectRepository.getOne(subjectId);
+		Page<Test> list = repository.getTestsFromStudentSubjectTeam(me, subject, team, pageable);
+		return list.map(x -> new TestDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public TestDTO findById(Long id) {
+		Optional<Test> obj = repository.findById(id);
+		Test entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		return new TestDTO(entity);
+	}
+
+	@Transactional
+	public TestDTO insert(TestDTO dto) {
+		Test entity = new Test();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new TestDTO(entity);
+	}
+
+	@Transactional
+	public TestDTO update(Long id, TestDTO dto) {
+		try {
+			Test entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new TestDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity Violation");
+		}
+	}
+	
+	private void copyDtoToEntity(TestDTO dto, Test entity) {
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setScore(dto.getScore());
+		entity.setStudent(userRepository.getOne(dto.getStudentId()));
+		entity.setSubject(subjectRepository.getOne(dto.getSubjectId()));
+	}
+}
