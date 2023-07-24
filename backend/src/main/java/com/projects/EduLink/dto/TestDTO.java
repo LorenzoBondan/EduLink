093242ae2aacd338f007980564845248440745c7
LorenzoBondan <lbbondan@gmail.com>
2023-07-24 10:39:47 -0300

    testDTO

---
+++ backend/src/main/java/com/projects/EduLink/dto/TestDTO.java
@@ -0,0 +1,106 @@
+package com.projects.EduLink.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.projects.EduLink.entities.Test;
+
+public class TestDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String name;
+	private Double score;
+	private LocalDateTime date;
+	private Long studentId;
+	private Long subjectId;
+	
+	public TestDTO() {}
+	
+	public TestDTO(Test entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.score = entity.getScore();
+		this.date = entity.getDate();
+		this.studentId = entity.getStudent().getId();
+		this.subjectId = entity.getSubject().getId();
+	}
+
+	public TestDTO(Long id, String name, Double score, LocalDateTime date, Long studentId, Long subjectId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.score = score;
+		this.date = date;
+		this.studentId = studentId;
+		this.subjectId = subjectId;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+	public Long getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(Long subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TestDTO other = (TestDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
