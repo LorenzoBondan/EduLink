c168e3d91645dd5347153162ba91c3042451bdf3
LorenzoBondan <lbbondan@gmail.com>
2023-07-24 15:16:34 -0300

    pre-authorize annotation

--- backend/src/main/java/com/projects/EduLink/resources/TestResource.java
+++ backend/src/main/java/com/projects/EduLink/resources/TestResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,6 +45,7 @@ public class TestResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@PreAuthorize("hasRole('ROLE_TEACHER')")
 	@PostMapping
 	public ResponseEntity<TestDTO> insert (@RequestBody TestDTO dto) {
 		dto = service.insert(dto);
@@ -52,12 +54,14 @@ public class TestResource {
 		return ResponseEntity.created(uri).body(dto);	
 	}
 	
+	@PreAuthorize("hasRole('ROLE_TEACHER')")
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<TestDTO> update(@PathVariable Long id, @RequestBody TestDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
+	@PreAuthorize("hasRole('ROLE_TEACHER')")
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<TestDTO> delete(@PathVariable Long id) {
 		service.delete(id);
