37b1807b3b229b23529250659b4c5dd8350df495
LorenzoBondan <lbbondan@gmail.com>
2023-07-25 17:28:48 -0300

    topnavbar

--- frontend/src/components/TopNavbar/index.tsx
+++ frontend/src/components/TopNavbar/index.tsx
@@ -1,12 +1,10 @@
-import { NavLink, useHistory } from 'react-router-dom';
-import { MdAdminPanelSettings } from 'react-icons/md';
+import { NavLink } from 'react-router-dom';
+import { MdOutlineAdminPanelSettings } from 'react-icons/md';
 import { IoIosNotificationsOutline } from 'react-icons/io';
-import { AiOutlinePlus } from 'react-icons/ai';
-import { TbArrowsExchange } from 'react-icons/tb';
+import { HiOutlineChatBubbleOvalLeft } from 'react-icons/hi2';
+import { TbClipboardData } from 'react-icons/tb';
 import { HiLogout } from 'react-icons/hi';
 import { HiLogin } from 'react-icons/hi';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { AiOutlineEdit } from 'react-icons/ai';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { User } from 'types';
 import { getTokenData, hasAnyRoles, isAuthenticated } from 'util/auth';
@@ -18,8 +16,7 @@ import logo from 'assets/images/logo.png';
 import { AuthContext } from 'AuthContext';
 import { removeAuthData } from 'util/storage';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
-import Modal from 'react-modal';
-
+import history from 'util/history';
 
 const TopNavbar = () => {
 
@@ -88,42 +85,27 @@ const TopNavbar = () => {
         }
     }
 
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    function openModal(){
-      setModalIsOpen(true);
-    }
-  
-    function closeModal(){
-      setModalIsOpen(false);
-    }
-
-    const history = useHistory();
-
-    const handlePush = () => {
-      history.push("/exchanges");
-      closeModal();
-    };
-
     return(
         <div className='top-navbar-main-container'>
-          <div className='exchanges-container-navbar'>
+          <div className='main-container-navbar'>
               <div className='top-navbar-brand'>
                 <img src={logo} alt="" />
-                <h4>EduLink</h4>
+                <h4>EDULINK</h4>
               </div>
               {authContextData.authenticated ?
               <>
-              <div className='exchanges-container-second'>
+              <div className='second-container-navbar'>
                   <ReactTooltip id="top-navbar-tooltip" place="bottom" />
                   <NavLink to="/profile" className='top-navbar-profile-container'>
-                      <img src={user?.imgUrl} alt=""/>
                       <h1>{user?.name}</h1>
+                      {user?.roles.map(role => (
+                        <p className='user-role-badge' key={role.id}>{role.authority.substring(5)}</p>
+                      ))}
                   </NavLink>
                   <div className='top-navbar-buttons-container'>
                     { hasAnyRoles(['ROLE_ADMIN']) && ( 
                       <NavLink to="/admin" className="admin-nav-item">
-                          <p data-tooltip-content="Admin" data-tooltip-id="top-navbar-tooltip"><MdAdminPanelSettings className='top-navbar-icon'/></p>
+                          <p data-tooltip-content="Admin" data-tooltip-id="top-navbar-tooltip"><MdOutlineAdminPanelSettings className='top-navbar-icon'/></p>
                       </NavLink>
                     )}
                     <p onClick={() => openAndCloseNotifications()} data-tooltip-content="Notifications" data-tooltip-id="top-navbar-tooltip">
@@ -131,11 +113,11 @@ const TopNavbar = () => {
                       {user && user?.notifications.filter(notification => !notification.read).length > 0 && <span className='notification-badge'>{user?.notifications.filter(notification => !notification.read).length}</span>}
                     </p>
 
-                    <NavLink to="/disponibleExchanges">
-                        <p data-tooltip-content="Disponible Exchanges" data-tooltip-id="top-navbar-tooltip"><AiOutlineSearch className='top-navbar-icon'/></p>
+                    <NavLink to="/messages">
+                        <p data-tooltip-content="Messages" data-tooltip-id="top-navbar-tooltip"><HiOutlineChatBubbleOvalLeft className='top-navbar-icon'/></p>
                     </NavLink>
-                    <NavLink to="/exchanges">
-                        <p data-tooltip-content="My Exchanges" data-tooltip-id="top-navbar-tooltip"><TbArrowsExchange className='top-navbar-icon'/></p>
+                    <NavLink to="/subjects">
+                        <p data-tooltip-content="Subjects" data-tooltip-id="top-navbar-tooltip"><TbClipboardData className='top-navbar-icon'/></p>
                     </NavLink>
                     <NavLink to="/" className="login-nav-item" onClick={handleLogoutClick}>
                       <p data-tooltip-content="Logout" data-tooltip-id="top-navbar-tooltip"><HiLogout className='top-navbar-icon'/></p>
@@ -145,7 +127,7 @@ const TopNavbar = () => {
               </> : (
                 <div className='top-navbar-login-container'>
                     <NavLink to="/auth/login" className="login-nav-item">
-                      <p style={{color:"#FF1493"}}><HiLogin/> Sign In</p>
+                      <p><HiLogin/> Sign In</p>
                     </NavLink>
                 </div>
               )}
