6bdb455a30989dcfe58114074fc3b330166e5a46
LorenzoBondan <lbbondan@gmail.com>
2023-07-25 21:29:29 -0300

    subjectCard

---
+++ frontend/src/pages/Subjects/SubjectCard/index.tsx
@@ -0,0 +1,57 @@
+import { Subject } from 'types';
+import { useCallback, useEffect, useState } from 'react';
+import './styles.css';
+import { requestBackend } from 'util/requests';
+import { AxiosRequestConfig } from 'axios';
+import { FaUsers } from 'react-icons/fa';
+import { HiOutlineDocumentText } from 'react-icons/hi2';
+
+type Props = {
+    subjectId: number;
+}
+
+const SubjectCard = ({subjectId} : Props) => {
+
+    const [subject, setSubject] = useState<Subject>();
+
+    const getSubject = useCallback(() => {
+        const params : AxiosRequestConfig = {
+          method:"GET",
+          url: `/subjects/${subjectId}`,
+          withCredentials:true
+        }
+        requestBackend(params) 
+          .then(response => {
+            setSubject(response.data);
+          })
+    }, [subjectId])
+
+    useEffect(() => {
+        getSubject();
+    }, [getSubject]);
+
+
+    return(
+        <div className='subject-card-container'>
+            <div className='subject-card-top-container'>
+                <img src="" alt="" />
+            </div>
+            <div className='subject-card-bottom-container'>
+                <h5>{subject?.name}</h5>
+                <p>{subject?.team} - {subject?.name}</p>
+            </div>
+            <div className='subject-card-buttons-container'>
+                <div className='subject-card-button'>
+                    <FaUsers/>
+                    <p>{subject?.students.length}</p>
+                </div>
+                <div className='subject-card-button'>
+                    <HiOutlineDocumentText/>
+                    <p>{subject?.tests.length}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SubjectCard;
--- frontend/src/pages/Subjects/index.tsx
+++ frontend/src/pages/Subjects/index.tsx
@@ -4,6 +4,8 @@ import { useCallback, useEffect, useState } from 'react';
 import { getTokenData, hasAnyRoles } from 'util/auth';
 import { AxiosRequestConfig } from 'axios';
 import { requestBackend } from 'util/requests';
+import SubjectCard from './SubjectCard';
+import { Link } from 'react-router-dom';
 
 const Subjects = () => {
 
@@ -37,37 +39,59 @@ const Subjects = () => {
             <div className='subjects-canva-container'>
                 {/* STUDENT */}
                 {hasAnyRoles(['ROLE_STUDENT']) && (
-                    <div className='subject-card'>
-                        <div className='row'>
-                            {user?.subjectsSubscribedId.map(subjectId => (
-                                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-3 recipes-column">
-                                    <p>{subjectId}</p>
-                                </div> 
-                            ))}
+                    <div className='col-lg-4'>
+                        <div className='subject-card base-card'>
+                            <h4>Your Subsribed Subjects</h4>
+                            <div className='row'>
+                                <div className='col-lg-6'>
+                                    {user?.subjectsSubscribedId.slice(0, Math.ceil(user.subjectsSubscribedId.length / 2)).map(subjectId => (
+                                        <Link to={`/subjects/${subjectId}`}>
+                                            <SubjectCard subjectId={subjectId} key={subjectId} />
+                                        </Link>
+                                    ))}
+                                </div>
+                                <div className='col-lg-6'>
+                                    {user?.subjectsSubscribedId.slice(Math.ceil(user.subjectsSubscribedId.length / 2)).map(subjectId => (
+                                        <Link to={`/subjects/${subjectId}`}>
+                                            <SubjectCard subjectId={subjectId} key={subjectId} />
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 )}
                 {/* PARENT */}
                 {hasAnyRoles(['ROLE_PARENT']) && (
-                    <div className='subject-card'>
-                        <div className='row'>
-                            {user?.childrenId.map(childrenId => (
-                                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-3 recipes-column">
-                                    <p>{childrenId}</p>
-                                </div> 
-                            ))}
+                    <div className='col-lg-4'>
+                        <div className='subject-card'>
+                            <div className='row'>
+                                {user?.childrenId.map(childrenId => (
+                                    <div className="col-sm-12 col-md-12 col-lg-6 col-xl-3 recipes-column">
+                                        <p>{childrenId}</p>
+                                    </div> 
+                                ))}
+                            </div>
                         </div>
                     </div>
                 )}
                 {/* TEACHER */}
                 {hasAnyRoles(['ROLE_TEACHER']) && (
-                    <div className='subject-card'>
-                        <div className='row'>
-                            {user?.subjectsTaughtId.map(subjectId => (
-                                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-3 recipes-column">
-                                    <p>{subjectId}</p>
-                                </div> 
-                            ))}
+                    <div className='col-lg-4'>
+                        <div className='subject-card base-card'>
+                            <h4>Your Taught Subjects</h4>
+                            <div className='row'>
+                                <div className='col-lg-6'>
+                                    {user?.subjectsTaughtId.slice(0, Math.ceil(user.subjectsTaughtId.length / 2)).map(subjectId => (
+                                        <p>{subjectId}</p>
+                                    ))}
+                                </div>
+                                <div className='col-lg-6'>
+                                    {user?.subjectsTaughtId.slice(Math.ceil(user.subjectsTaughtId.length / 2)).map(subjectId => (
+                                        <p>{subjectId}</p>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 )}
