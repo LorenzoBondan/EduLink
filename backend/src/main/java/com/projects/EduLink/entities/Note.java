8f5161c6ddd9a0ce7c6e613b1b77b09f91180bf9
LorenzoBondan <lbbondan@gmail.com>
2023-07-26 17:46:03 -0300

    added class Note

---
+++ backend/src/main/java/com/projects/EduLink/entities/Note.java
@@ -0,0 +1,106 @@
+package com.projects.EduLink.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_note")
+public class Note implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(columnDefinition = "TEXT")
+	private String text;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private LocalDateTime moment;
+	
+	@ManyToOne
+	@JoinColumn(name = "teacher_id")
+	private User teacher;
+	
+	@ManyToOne
+	@JoinColumn(name = "subject_id")
+	private Subject subject;
+	
+	public Note() {}
+
+	public Note(Long id, String text, LocalDateTime moment, User teacher, Subject subject) {
+		super();
+		this.id = id;
+		this.text = text;
+		this.moment = moment;
+		this.teacher = teacher;
+		this.subject = subject;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public LocalDateTime getMoment() {
+		return moment;
+	}
+
+	public void setMoment(LocalDateTime moment) {
+		this.moment = moment;
+	}
+
+	public User getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(User teacher) {
+		this.teacher = teacher;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Note other = (Note) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
