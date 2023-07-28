b17110a858f2334a1e990d8faf632c4a26fff18b
LorenzoBondan <lbbondan@gmail.com>
2023-07-28 11:43:46 -0300

    fixed table width

--- frontend/src/pages/SubjectDetails/index.tsx
+++ frontend/src/pages/SubjectDetails/index.tsx
@@ -206,7 +206,7 @@ const SubjectDetails = () => {
                                         )}
                                     </tr>
                                 </thead>
-                                <tbody style={{width:"100%", display:"flex", flexDirection:"column"}}>
+                                <tbody>
                                     {/* STUDENT */}
                                     {hasAnyRoles(['ROLE_STUDENT']) && (
                                         subject && tests?.content.map(test => (
